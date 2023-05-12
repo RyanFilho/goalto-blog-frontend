08cdb63e0629103fea1f14731f84522db5dda7ea
Ryan Filho <ryan.filho@outlook.com>
2023-05-12 20:19:03 -0300

    feat(logo): Add redirect to logo

--- src/components/Atoms/Logo/index.tsx
+++ src/components/Atoms/Logo/index.tsx
@@ -1,9 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import './index.scss';
 
 export function Logo() {
+  const navigate = useNavigate();
   return (
     <div className="logo-container">
-      <svg viewBox="0 0 400 90" xmlns="http://www.w3.org/2000/svg">
+      <svg viewBox="0 0 400 90" xmlns="http://www.w3.org/2000/svg" onClick={() => navigate("/")}>
         <text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle" fontSize="48">
           Lorem ipsum!
         </text>
